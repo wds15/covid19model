01a1414563b5138fe24fc2f1a00170ebdf752224
Melodie Monod <monod.melodie@gmail.com>
2020-05-07 16:57:21 +0100

    first commit

---
+++ base-ages.r
@@ -0,0 +1,111 @@
+library(rstan)
+library(data.table)
+library(gdata)
+library(EnvStats)
+
+setwd("~/git/covid19model")
+
+## command line parsing if any
+args <- list(
+  stanModelFile= 'base_age_google_mobility_200427',
+  seed= 42,
+  chain= 1,
+  indir= '~/git/covid19model',
+  outdir= '~/sandbox/covid19model-2.0/results',
+  job_tag= '14countries',
+  cntct_by = 5,
+  seedAge = 7L
+)
+
+## set other args
+args$file_stanModel <- file.path(args$indir,'stan-models',paste0(args$stanModelFile,'.stan'))
+args$file_serial_interval <- file.path(args$indir,'data','serial_interval.csv')
+args$file_covariates <- file.path(args$indir,'data','interventions.csv')
+args$file_contact <- file.path(args$indir,'data','contact_UK.rda') # with weekday weekend
+args$file_pop_age <- file.path(args$indir,'data','popByAge.csv')
+args$file_age_ifr <- file.path(args$indir,'data','ifr_age.csv')
+args$file_google_mobility <- file.path(args$indir,'data','google-mobility.csv')
+tmp <- Sys.getenv("PBS_JOBID")
+args$job_id <- ifelse(tmp!='', tmp, as.character(abs(round(rnorm(1) * 1e6))) )
+args$job_dir <- file.path(args$outdir,paste0(args$stanModelFile,'-',args$job_tag,'-',args$job_id)) 
+args$DEBUG <- FALSE
+
+## start script
+cat(sprintf("Running\n"))
+str(args)
+set.seed(args$seed)
+
+source(file.path(args$indir, "utils", "read-data.r"))
+source(file.path(args$indir, "utils", "process-covariates.r"))
+
+## make job dir
+dir.create( args$job_dir )
+
+## save input args
+saveRDS( args, file=file.path(args$job_dir, paste0(basename(args$job_dir), '_args.RDS')))
+
+# countries to investigate
+countries <- read.csv('data/regions.csv', stringsAsFactors = FALSE)
+
+## Reading all data
+d <- read_obs_data(countries)
+
+# Read ifr 
+ifr.by.country <- read_ifr_data()
+
+#	read age-specific ifr
+ifr.by.age = read.csv(args$file_age_ifr)
+
+#	read contact rates
+load(args$file_contact)
+
+#	read age-specific pop counts
+pop_by_age = read.csv(args$file_pop_age)
+
+# Read interventions
+covariates <- read_interventions(args$file_covariates)
+
+# Read serial interval
+serial.interval <- read.csv(args$file_serial_interval)
+
+# read google mobility
+google_mobility <- read.csv(args$file_google_mobility)
+
+# Process covariates
+forecast <- 0
+N2 <- 90 # increase if you need more forecast
+
+processed_data <- process_covariates_age( countries$Regions, 
+                                         google_mobility, 
+                                         d, 
+                                         ifr.by.country, 
+                                         ifr.by.age, 
+                                         serial.interval, 
+                                         pop_by_age, 		 
+                                         contact_tab, 
+                                         args$seedAge, 
+                                         N2, 
+                                         forecast)
+stan_data = processed_data$stan_data
+dates = processed_data$dates
+deaths_by_country = processed_data$deaths_by_country
+reported_cases = processed_data$reported_cases
+
+## save image before running Stan
+save.image( file=file.path(args$job_dir, paste0(basename(args$job_dir), '_stanin.RData')) )
+
+## run Stan
+cat('\nRunning Stan... \n')
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+model <- stan_model(args$file_stanModel)
+
+if(args$DEBUG) {
+  fit <- sampling(model,data=stan_data,iter=10,warmup=5,chains=1,seed=args$seed,verbose=TRUE)
+} else { 
+  # uncomment the line below for a full run to replicate results and comment the second line below 
+  fit <- sampling(model,data=stan_data,iter=3000, warmup=2000, chains=1,seed=args$seed, thin=1, control = list(adapt_delta = 0.95, max_treedepth = 15))
+  #fit = sampling(model,data=stan_data,iter=10,warmup=5,chains=1,seed=args$seed,thin=1,control = list(adapt_delta = 0.95, max_treedepth = 10))
+}  
+
+save(fit, file=file.path(args$job_dir, paste0(basename(args$job_dir), '_stanout.RData')) )
--- utils/process-covariates.r
+++ utils/process-covariates.r
@@ -130,3 +130,183 @@ process_covariates <- function(countries, interventions, d, ifr.by.country,N2){
   }
   return(list("stan_data" = stan_data, "dates" = dates, "reported_cases"=reported_cases, "deaths_by_country" = deaths_by_country))
 }
+
+process_covariates_age <- function(countries, google_mobility, d, ifr.by.country, ifr.by.age, serial.interval, pop_by_age, contact_tab, seedAge, N2, forecast)
+{
+  # various distributions required for modeling
+  set.seed(5678) # make reproducible 
+  mean1 <- 5.1; cv1 <- 0.86; # infection to onset
+  mean2 <- 18.8; cv2 <- 0.45 # onset to death
+  x1 <- rgammaAlt(1e6,mean1,cv1) # infection-to-onset distribution
+  x2 <- rgammaAlt(1e6,mean2,cv2) # onset-to-death distribution
+  ecdf_death <- ecdf(x1+x2)
+  
+  #	disrectise to days since infection
+  ifr_by_dsi <- vector('numeric',N2)
+  ifr_by_dsi[1] <- ecdf_death(1.5) - ecdf_death(0)
+  for(s in 2:N2)
+  {
+    ifr_by_dsi[s] <- ecdf_death(s+.5) - ecdf_death(s-.5)
+  }
+  
+  # serial intervention cut
+  serial.interval.cut <- length( which(serial.interval$fit * 5e6 >= 1 ) )
+  
+  # contact bands
+  cntct_bands <- nrow(contact_tab[[1]][[1]]) 
+  
+  # going over each region
+  dates <- list()
+  reported_cases <- list()
+  deaths_by_country <- list()
+  stan_data <- list(
+    #	constants
+    M = length(countries), 									# int<lower=1> M		number of countries
+    N0 = 6L, 												# int<lower=1> N0		N0 = 6 to make it consistent with Rayleigh
+    N = vector('integer',length(countries)),				# int<lower=1> N[M]		days of observed data for country m. each entry must be <= N2
+    N2 = as.integer(N2), 									# int<lower=1> N2		days of observed data + # of days to forecast
+    A = as.integer(cntct_bands), 							# int<lower=1> A		number of age bands
+    SI_CUT = serial.interval.cut, 							# int<lower=1> SI_CUT	number of days in serial interval to consider
+    P_RES = 1L,												#int<lower=1> P_RES; 	number of predictors for residential contacts		
+    P_NONRES = 4L,											# int<lower=1> P_NONRES number of predictors for non-residential contacts
+    WKEND_IDX_N= vector('integer',length(countries)),		# int WKEND_IDX_N[M];	number of weekend indices in each location
+    #	data
+    pop = vector('numeric',length(countries)),				# real pop[M];									population counts
+    popByAge = matrix(NA_integer_, as.integer(cntct_bands), length(countries)),		# matrix<lower=0, upper=1>[A,M] popByAge	proportion of age bracket in population in location
+    epidemicStart = vector('integer',length(countries)), 	# int epidemicStart[M];		
+    deaths = matrix(NA_integer_, N2, length(countries)), 	# int deaths[N2, M];	 						reported deaths
+    wkend_idx = matrix(NA_integer_, N2, length(countries)),	# int<lower=1> wkend_idx[N2,M]; 				indices of 1:N2 that correspond to weekends in location m
+    covariates_nonres = vector('list', length(countries)),	# matrix[N2,P_NONRES]  covariates_nonres[M]		predictors for non-residential contacts
+    covariates_res = vector('list', length(countries)),		# matrix[N2,P_RES] covariates_res[M]			predictors for residential contacts
+    #	priors
+    cntct_weekdays_mean = vector('list', length(countries)), 								# matrix[A,A] cntct_mean[M]						mean of prior contact rates between age groups
+    cntct_weekends_mean = vector('list', length(countries)), 
+    ifr_country_scale = vector('numeric', length(countries)),						# real<lower=0> ifr_country_scale[M]; 			relative probability of death for location, s days after infection, for age band a 
+    ifr_age = matrix(data=ifr.by.age$ifr_mean, ncol=cntct_bands, nrow=N2, byrow=TRUE), 	# matrix<lower=0>[N2,A] ifr_age; 				probability of death for age band a, stored N2 times 
+    hyperpara_ifr_age = ifr.by.age[, c("alpha", "beta")],
+    rev_ifr_daysSinceInfection = rev( ifr_by_dsi ), 									# row_vector[N2] rev_ifr_daysSinceInfection;	probability of death s days after infection in reverse order
+    rev_serial_interval = rev( serial.interval$fit[1:serial.interval.cut] ), 			# row_vector[SI_CUT] rev_serial_interval;		fixed pre-calculated serial interval using empirical data from Neil in reverse order
+    init_A = ifelse(cntct_bands==101, 30L, ifelse(cntct_bands==20, as.numeric(seedAge), NA_integer_))	# int<lower=1, upper=A> init_A					age band in which initial cases occur in the first N0 days 
+  ) 		
+  
+  cat('\nProcessing country data... \n')
+  for(m in seq_along(countries)) 
+  {
+    #m <- 1	
+    Country <- countries[m]
+    
+    #	create padded data of cases and deaths
+    # start counting 30 days prior to reaching 10 deaths
+    d1 <- d[d$Country==Country,]
+    d1$date <- as.Date(d1$DateRep,format='%Y-%m-%d')
+    d1$t <- decimal_date(d1$date) 
+    d1 <- d1[order(d1$t),]
+    date_min <- dmy('31/12/2019') 
+    if (as.Date(d1$DateRep[1], format='%Y-%m-%d') > as.Date(date_min, format='%Y-%m-%d')){
+      print(paste(Country,'In padding'))
+      pad_days <- as.Date(d1$DateRep[1], format='%Y-%m-%d') - date_min
+      pad_dates <- date_min + days(1:pad_days[[1]]-1)
+      padded_data <- data.frame("Country" = rep(Country, pad_days),
+                                "DateRep" = format(pad_dates, '%Y-%m-%d'),
+                                "t" = decimal_date(as.Date(pad_dates,format='%Y-%m-%d')),
+                                "date" = as.Date(pad_dates,format='%Y-%m-%d'),
+                                "Cases" = as.integer(rep(0, pad_days)),
+                                "Deaths" = as.integer(rep(0, pad_days)),
+                                stringsAsFactors=F)
+      padded_data$DateRep = as.Date(padded_data$DateRep, format='%Y-%m-%d')
+      d1 <- bind_rows(padded_data, d1)
+    }
+    index <- which(d1$Cases>0)[1]
+    index1 <- which(cumsum(d1$Deaths)>=10)[1] # also 5
+    index2 <- index1-30L
+    print(sprintf("First non-zero cases is on day %d, and 30 days before 10 deaths is day %d",index,index2))
+    d1 <- d1[index2:nrow(d1),]
+    
+    #	add data on mobility indices
+    google_mobility$date = as.Date(google_mobility$date,format='%Y-%m-%d')
+    google_mobility$ISO_name = as.character(google_mobility$ISO_name)
+    tmp <- google_mobility %>% 
+      select(-c(ISO_code,county)) %>% 
+      rename(Country:=ISO_name)
+    d1 <- d1 %>% 
+      group_by(Country,date) %>% 
+      left_join(tmp) %>% 
+      ungroup()
+    for(x in c("grocery.pharmacy", "parks", "residential", "retail.recreation","transitstations","workplace"))
+    {
+      d1[[x]]
+      z <- which(is.na(d1[[x]]))			
+      z2 <- which(diff(z)!=1)
+      if(!length(z2)) 
+        z2 <- length(z)
+      d1[[x]][1:z2] <- 0
+      z <- which(is.na(d1[[x]]))
+      if(length(z)) 
+        d1[[x]][z] <- d1[[x]][z[1]-1] 
+    }
+    
+    #	determine the index of weekend dates
+    wkend_idx <- which(as.integer(format(d1$date, "%u"))>5)
+    
+    # store dates for data from this country
+    dates[[Country]] <- d1$date  
+    
+    # hazard estimation
+    N <- nrow(d1)
+    print(sprintf("%s has %d days of data",Country,N))
+    forecast <- N2 - N
+    if(forecast < 0) {
+      print(sprintf("%s: %d", Country, N))
+      print("ERROR!!!! increasing N2")
+      N2 <- N
+      forecast <- N2 - N
+    }
+    
+    #	calculate probability of deaths in age a after s days of infection for this country 
+    # to obtain ifr.by.age.country, I scale here ifr.by.age with the ratio ifr.by.country/ifr.china
+    # where ifr.china is 0.657% as in Verity 2020
+    # TODO this will likely need to be revised
+    ifr_china <- 0.00657
+    ifr_country_scale <- ifr.by.country$ifr[ifr.by.country$country == Country] / ifr_china  
+    
+    reported_cases[[Country]] <- as.vector(as.numeric(d1$Cases))
+    deaths <- c(as.vector(as.numeric(d1$Deaths)),rep(-1,forecast))
+    cases <- c(as.vector(as.numeric(d1$Cases)),rep(-1,forecast))
+    deaths_by_country[[Country]] <- as.vector(as.numeric(d1$Deaths))
+    covariates <- d1 %>% select(-c(DateRep, Cases, Deaths, Country, date, t))
+    covariates[N:(N+forecast),] <- covariates[N,]
+    
+    #	add constants
+    stan_data$N[m] <- N
+    stan_data$WKEND_IDX_N[m] <- length(wkend_idx)
+    stan_data$wkend_idx[,m] <- c(wkend_idx, rep(0,N2-length(wkend_idx)))
+    
+    # add data  
+    stan_data$pop[m] <- ifr.by.country$popt[ifr.by.country$country==Country]
+    stan_data$popByAge[,m] <- pop_by_age[,Country]
+    stan_data$epidemicStart[m] <- index1+1L-index2	#TODO confirm, this is always 31?
+    stan_data$deaths[,m] <- deaths
+    stan_data$covariates_res[[m]] <- as.matrix( covariates %>% select(residential) )
+    stan_data$covariates_nonres[[m]] <- as.matrix( covariates %>% select(parks, retail.recreation, transitstations, workplace) )		
+    
+    # add priors				
+    stan_data$cntct_weekends_mean[[m]] <- as.matrix(contact_tab[["United_Kingdom"]][["weekend"]])
+    stan_data$cntct_weekdays_mean[[m]] <- as.matrix(contact_tab[["United_Kingdom"]][["weekday"]])
+    
+    stan_data$ifr_country_scale[m] <- ifr_country_scale
+    
+    if(length(stan_data$N) == 1) 
+    {
+      stan_data$N <- as.array(stan_data$N)
+    }
+  }
+  
+  # transform in array single vector 
+  if(stan_data$M == 1){
+    stan_data$pop = as.array(stan_data$pop)
+    stan_data$epidemicStart = as.array(stan_data$epidemicStart)
+    stan_data$ifr_country_scale = as.array(stan_data$ifr_country_scale)
+  }
+  
+  return(list("stan_data" = stan_data, "dates" = dates, "reported_cases"=reported_cases, "deaths_by_country" = deaths_by_country))
+} 
